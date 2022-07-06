4eb3c5aa5a6d1419724cbda16e46e8b56800a895
Jim Belcher <jbelcher1985@yahoo.com>
2022-07-06 19:01:58 -0400

    initial commit

---
+++ api/src/functions/hello/handler.ts
@@ -0,0 +1,14 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+
+import schema from './schema';
+
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  return formatJSONResponse({
+    message: `Hello Jim, welcome to the exciting Serverless world! ${process.env.TESTER}`,
+    event,
+  });
+};
+
+export const main = middyfy(hello);
