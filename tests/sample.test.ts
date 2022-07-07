7325a4b359fc46500a5d9c83ebda08850485b9f1
Jim Belcher <jbelcher1985@yahoo.com>
2022-07-06 22:05:53 -0400

    lcov only

--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -1,7 +1,13 @@
 import { expect } from 'chai';
+import Test from '../src/functions/test'
 
 describe('calculate', function() {
     it('add', function() {
       expect(true).equal(true);
     }); 
-  });
+
+    it('test', () => {
+      let test = new Test()
+      expect(test.test()).equal(2)
+    })
+});
